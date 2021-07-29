ab8548178e0209b32170371f968e66f1c027bc74
Thanh Minh Nguyen <nguyenthanhminh112000@gmail.com>
2021-07-29 18:45:02 +0700

    Config firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: 'AIzaSyBBq7jDu_kbP1shxBov-jUiYs-5gDsFFx8',
+  authDomain: 'cloudsbay-c1dda.firebaseapp.com',
+  projectId: 'cloudsbay-c1dda',
+  storageBucket: 'cloudsbay-c1dda.appspot.com',
+  messagingSenderId: '362992299749',
+  appId: '1:362992299749:web:0994ca9bd9c4ae9f81ee0c',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();

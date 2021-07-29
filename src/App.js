4a1160a2aaa4dabd9fb2c35f883d75b70c6cda3b
Thanh Minh Nguyen <nguyenthanhminh112000@gmail.com>
2021-07-29 17:17:27 +0700

    Create Header component,download and set-up antd,@ant-design/icons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/nav/Header';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Home from './pages/Home';
 const App = () => {
   return (
-    <div className='App'>
+    <>
+      <Header />
       <Switch>
         <Route path='/login' exact component={Login} />
         <Route path='/register' exact component={Register} />
         <Route path='/' component={Home} />
       </Switch>
-    </div>
+    </>
   );
 };
 

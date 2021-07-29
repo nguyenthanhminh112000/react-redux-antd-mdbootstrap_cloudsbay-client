34e3e8adb4e8ee872c90f9a8cf6f6bea4e72ebd9
Thanh Minh Nguyen <nguyenthanhminh112000@gmail.com>
2021-07-29 15:15:30 +0700

    Routes and Pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Home from './pages/Home';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Switch>
+        <Route path='/login' exact component={Login} />
+        <Route path='/register' exact component={Register} />
+        <Route path='/' component={Home} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;

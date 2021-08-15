6823a54d10321b107ced421695978dd7fa5d6e8b
Thanh Minh Nguyen <nguyenthanhminh112000@gmail.com>
2021-08-15 17:29:38 +0700

    Re-layout Header

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -12,29 +12,53 @@ console.log('Header outside');
 const Header = () => {
   console.log('Header inside');
   //hooks
-  const [current, setCurrent] = useState('home');
+  const [current, setCurrent] = useState('');
   //functions
   const handleClick = (e) => {
     setCurrent(e.key);
   };
   return (
-    <Menu onClick={handleClick} selectedKeys={[current]} mode='horizontal'>
-      {console.log('Header inside return')}
-      <Item key='home' icon={<AppstoreOutlined />}>
-        <Link to='/'>Home</Link>
-      </Item>
-
-      <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Username'>
-        <Item key='setting:1'>Option 1</Item>
-        <Item key='setting:2'>Option 2</Item>
-      </SubMenu>
-      <Item key='register' icon={<UserAddOutlined />} className='float-right'>
-        <Link to='/register'> Register</Link>
-      </Item>
-      <Item key='login' icon={<UserOutlined />} className='float-right'>
-        <Link to='/login'> Login</Link>
-      </Item>
-    </Menu>
+    <nav
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        borderBottom: '1px solid #f0f0f0',
+        zIndex: '9',
+      }}
+    >
+      <nav>
+        <Menu
+          onClick={handleClick}
+          selectedKeys={[current]}
+          mode='horizontal'
+          style={{ display: 'flex' }}
+        >
+          {console.log('Header inside return')}
+          <Item key='home' icon={<AppstoreOutlined />}>
+            <Link to='/'>Home</Link>
+          </Item>
+          <SubMenu key='SubMenu' icon={<SettingOutlined />} title='Username'>
+            <Item key='setting:1'>Option 1</Item>
+            <Item key='setting:2'>Option 2</Item>
+          </SubMenu>
+        </Menu>
+      </nav>
+      <nav>
+        <Menu
+          onClick={handleClick}
+          selectedKeys={[current]}
+          mode='horizontal'
+          style={{ display: 'flex' }}
+        >
+          <Item key='login' icon={<UserOutlined />}>
+            <Link to='/login'> Login</Link>
+          </Item>
+          <Item key='register' icon={<UserAddOutlined />}>
+            <Link to='/register'> Register</Link>
+          </Item>
+        </Menu>
+      </nav>
+    </nav>
   );
 };
 
